4644f16c1268af1415d456f4eff1900f1b99ffd0
MR_H-u-m-A-N <165131331+Pelmeni6@users.noreply.github.com>
2026-09-18 18:31:09 +0500

    ABC 456

---
+++ ABC/A/Dice.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll x;
+    cin >> x;
+    if (x >= 3 && x <= 18) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
